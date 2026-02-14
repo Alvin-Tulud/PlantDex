1dc58ba82cbeb6bf0450d91dd9188ccb07674ec4
alee84sf <100506073+alee84sf@users.noreply.github.com>
2026-02-14 01:03:24 -0800

    added express.static to make /public/css work

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 // Index route
 import indexRouter from "./routes/index.js";
