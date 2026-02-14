75f8bfb615db302a84ca80031f0a5ebbb898c818
Alvin-Tulud <alvintulud@gmail.com>
2026-02-13 21:52:28 -0800

    figuring out why tf tailwindcss is not loading

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import express from "express"
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 // Index route
 import indexRouter from './routes/index.js';
 app.use('/', indexRouter);
