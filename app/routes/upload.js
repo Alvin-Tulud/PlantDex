b8641657ac765353a58f9f8703ebd83d62c02259
alee84sf <100506073+alee84sf@users.noreply.github.com>
2026-02-15 10:37:02 -0800

    gemini upload has description now

--- app/routes/upload.js
+++ app/routes/upload.js
@@ -42,8 +42,8 @@ router.post("/",guardrail, upload.single("image"), async (req, res) => {
 
         // To make a new entry I need
         // common_name, scientific_name, edible, coordinates, addition_date (CURRENT_DATE), image
-        const result = await updateTable( "INSERT INTO plant (fk_user_id, common_name, scientific_name, edible, coordinates, addition_date, image) VALUES (?, ?, ?, ?, ?, CURRENT_DATE, ?)", 
-            [req.session.user.user_id, plantData.commonName, plantData.scientificName, plantData.Edible, plantData["nativeTo(Lat,long)"], req.file.filename]);
+        const result = await updateTable( "INSERT INTO plant (fk_user_id, common_name, scientific_name, description, edible, coordinates, addition_date, image) VALUES (?, ?, ?, ?, ?, ?, CURRENT_DATE, ?)", 
+            [req.session.user.user_id, plantData.commonName, plantData.scientificName, plantData.Description, plantData.Edible, plantData["nativeTo(Lat,long)"], req.file.filename]);
 
         return res.redirect(303, "/");
 
