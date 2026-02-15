80a7e5ca95dd92e9d05c3e0d7e1d86b795cce1d3
alee84sf <100506073+alee84sf@users.noreply.github.com>
2026-02-15 09:55:42 -0800

    might need express.json() for session to get userid

--- app/routes/login.js
+++ app/routes/login.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
     res.render(path.join(__dirname, "../views/login.ejs"));
 });
 
-router.post("/", login, async (req, res) => {
+router.post("/", express.json(), login, async (req, res) => {
     console.log(req.session.user);
     res.redirect("/");
 });
