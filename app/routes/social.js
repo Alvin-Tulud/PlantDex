b0303beb63c7ee5c509b27de577ffe16cd3d1bde
UngDavid730 <davidthinung@gmail.com>
2026-02-15 05:37:02 -0800

    login routing handled - login routing doesnt hang after creating a new account it will route u back to home page

--- app/routes/social.js
+++ app/routes/social.js
@@ -4,13 +4,14 @@ import path from "path";
 
 import { fileURLToPath } from "url";
 import { queryTable } from "../services/db.js";
+import { guardrail } from "../middleware/loginFunctions.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // home route
 
-router.get("/", async (req, res) => {
+router.get("/",guardrail, async (req, res) => {
   const id = req.session.user.user_id;
   const plants = await queryTable("SELECT * FROM plant JOIN user ON plant.fk_user_id = user.user_id WHERE NOT fk_user_id = ? ORDER BY addition_date DESC", [id]);
   // debugging
