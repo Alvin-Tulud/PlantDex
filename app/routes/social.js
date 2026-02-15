3ea417b556c85000df5d04f43dd16f34286a2cb5
Alvin-Tulud <alvintulud@gmail.com>
2026-02-14 16:34:51 -0800

    added socials ejs

---
+++ app/routes/social.js
@@ -0,0 +1,21 @@
+import express from "express";
+const router = express.Router();
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// home route
+
+router.get("/", async (req, res) => {
+  res.render(path.join(__dirname, "../views/social.ejs"));
+});
+
+/*
+for req do a sql call for user after they have logged in check if logged in 
+then render all their plants from their id
+*/
+
+export default router;
