23e02cabd3409330ffe547852eb93ac57be641aa
alee84sf <100506073+alee84sf@users.noreply.github.com>
2026-02-15 04:37:08 -0800

    forgot to actually change it

--- app/routes/social.js
+++ app/routes/social.js
@@ -11,8 +11,8 @@ const __dirname = path.dirname(__filename);
 // home route
 
 router.get("/", async (req, res) => {
-  // TODO: REPLACE [1] with the actual userid
-  const plants = await queryTable("SELECT * FROM plant WHERE NOT fk_user_id = ? ORDER BY addition_date DESC", [1]);
+  const id = req.session.user.user_id;
+  const plants = await queryTable("SELECT * FROM plant WHERE NOT fk_user_id = ? ORDER BY addition_date DESC", [id]);
   // debugging
   console.log(plants);
   res.render(path.join(__dirname, "../views/social.ejs"),
