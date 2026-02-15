f4f3e1ef812bfb921cc3d0525ff2134903d03227
alee84sf <100506073+alee84sf@users.noreply.github.com>
2026-02-14 19:31:57 -0800

    adding db functionality, not done yet tho

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import path from "path";
 
 import { fileURLToPath } from "url";
+import { queryTable } from "../services/db.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,7 +11,12 @@ const __dirname = path.dirname(__filename);
 // home route
 
 router.get("/", async (req, res) => {
-  res.render(path.join(__dirname, "../views/home.ejs"));
+  const plants = queryTable("SELECT * FROM plant WHERE fk_user_id = ? ORDER BY addition_date DESC", [12]);
+  res.render(path.join(__dirname, "../views/home.ejs"),
+    {
+      plants: plants
+    }
+  );
 });
 
 /*
---
+++ app/services/db.js
@@ -0,0 +1,76 @@
+//import dotenv from "dotenv";
+import mysql from "mysql2";
+
+// Returns query from database
+// (currently prints instead of returning anything, will change later)
+export async function queryTable(query, values)
+{
+  let connection;
+  try {
+    // Create connection
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    });
+    connection.addListener('error', (err) => {
+      if (err instanceof Error) {
+        console.log(`createConnection error:`, err);
+      }
+    });
+
+    // Query the db
+    // values should be an [array], I think something breaks otherwise
+    const result = await connection.promise().query(query, values);
+    //console.log(result[0]);
+    console.log("Finished query");
+    return(result[0]);
+  } catch (err) {
+    console.error("Error querying db:", err.message);
+  } finally {
+    if (connection) {
+      await connection.end();  // Close the connection
+      console.log("Connection closed");
+    }
+  }
+}
+//queryTable("SELECT * FROM plant WHERE fk_user_id = ? ORDER BY addition_date DESC", [1]);
+
+
+
+// Updates table
+// (Are we supposed to return anything?)
+export async function updateTable(query, values)
+{
+  let connection;
+  try {
+    // Create connection
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    });
+    connection.addListener('error', (err) => {
+      if (err instanceof Error) {
+        console.log(`createConnection error:`, err);
+      }
+    });
+
+    // Modify the db
+    // values should be an [array], I think something breaks otherwise
+    const result = await connection.execute(query, values);
+    console.log("Finished executing - check for error logs");
+  } catch (err) {
+    console.error("Error modifying db:", err.message);
+  } finally {
+    if (connection) {
+      await connection.end();  // Close the connection
+      console.log("Connection closed");
+    }
+  }
+}
+//updateTable("INSERT INTO user (username) VALUES (?)", ["testuser128"]);
